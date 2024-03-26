176b95032cc1493205f1075ddd2b6fd7534e804d
Matthieu VANCAYZEELE <112880161+matthieuv-chabe@users.noreply.github.com>
2024-03-26 11:05:04 +0000

    putazin

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
     poweredByHeader: false,
     reactStrictMode: false,
+    exportPathMap: async function(
+        defaultPathMap,
+        {dev,server,outDir,distDir,buildId}
+        ) {
+            return {}
+        }
 }
 
 module.exports = nextConfig
